54689c10dc1d233eabe608967d199f31b38b4219
Scuared <johnlmead89@gmail.com>
2022-10-27 10:52:26 -0400

    Randomization

--- memorygame.js
+++ memorygame.js
@@ -50,15 +50,14 @@ function isMatch(card1, card2){
 };
 function reset(){
     score = 0;
-    let rows = [];
-    let columns = [];
-    let row = 0;
-    let column = 0;
+    
     for(let i = 0; i < animalcards.length; i++){
         animalcards[i].style.display = "none";
         cardbacks[i].style.display = "block";
+        buttons[i].disabled = false;
     }
     scoreDisplay.style.color = "black";
+    randomize();
     drawScore();
 };
 function drawScore(){
@@ -67,5 +66,37 @@ function drawScore(){
     if(score == 6){
         scoreDisplay.innerHTML = "You Win!";
         scoreDisplay.style.color = "blue";
+    }
+};
+function randomize(){
+    let pos = [1,2,3,4,5,6,7,8,9,10,11,12];
+    for(let i = 0;i < 12;i++){
+        let rand = pos.splice([Math.floor(Math.random()* pos.length)], 1);
+        console.log(rand);
+        if(rand[0] == 1){
+            buttons[i].style.gridArea = '1 / 1'
+        }else if(rand[0] == 2){
+            buttons[i].style.gridArea = '1 / 2'
+        }else if(rand[0] == 3){
+            buttons[i].style.gridArea = '1 / 3'
+        }else if(rand[0] == 4){
+            buttons[i].style.gridArea = '1 / 4'
+        }else if(rand[0] == 5){
+            buttons[i].style.gridArea = '2 / 1'
+        }else if(rand[0] == 6){
+            buttons[i].style.gridArea = '2 / 2'
+        }else if(rand[0] == 7){
+            buttons[i].style.gridArea = '2 / 3'
+        }else if(rand[0] == 8){
+            buttons[i].style.gridArea = '2 / 4'
+        }else if(rand[0] == 9){
+            buttons[i].style.gridArea = '3 / 1'
+        }else if(rand[0] == 10){
+            buttons[i].style.gridArea = '3 / 2'
+        }else if(rand[0] == 11){
+            buttons[i].style.gridArea = '3 / 3'
+        }else if(rand[0] == 12){
+            buttons[i].style.gridArea = '3 / 4'
+        }
     }
 };
